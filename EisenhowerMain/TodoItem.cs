815053d85e1bcf11cadc2e7ed842863449b34721
Hacker-Wolf <alex.krysiuk@icloud.com>
2023-04-17 18:55:01 +0200

    TodoItem class creation

--- EisenhowerMain/TodoItem.cs
+++ EisenhowerMain/TodoItem.cs
@@ -1,10 +1,47 @@
 using System;
 
-namespace EisenhowerCore {
+namespace EisenhowerMain {
 public class TodoItem
     {
+        // Attributes
+        private string Title;
+        private DateTime Deadline;
+        private bool IsDone;
+        
+        //Constructor
+        public TodoItem(string Title, DateTime Deadline, bool IsDone)
+        {
+            this.Title = Title;
+            this.Deadline = Deadline;
+            this.IsDone = IsDone;
+        }
 
+        //Instance Methods
+        public string GetTitle()
+        {
+            return Title;
+        }
 
+        public DateTime GetDeadLine()
+        {
+            return Deadline;
+        }
+
+        public void Mark()
+        {
+            IsDone = true;
+        }
+
+        public void Unmark()
+        {
+            IsDone = false;
+        }
+
+        public override string ToString()
+        {
+            string doneStatus = IsDone ? "[x]" : "[ ]";
+            string formatedDeadline = Deadline.ToString("d-M");
+            return $"{doneStatus} {formatedDeadline} {Title}";
+        }
     }
-
 }
